6407e16330613b722f99e0153c778712a9d1f4fd
Solo800606 <61974103+Solo800606@users.noreply.github.com>
2024-07-23 18:40:32 +0300

    Добавлена логика по работе с файлами

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -6,9 +7,27 @@ public class Main {
         int firstNumber = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число:");
         int secondNumber = new Scanner(System.in).nextInt();
-        System.out.println("Сумма чисел:"+(firstNumber+secondNumber));
-        System.out.println("Разность чисел:"+(firstNumber-secondNumber));
-        System.out.println("Произведение чисел:"+(firstNumber*secondNumber));
-        System.out.println("Частное чисел:"+((double) firstNumber/secondNumber));
+//Вывод результата
+        System.out.println("Сумма чисел:" + (firstNumber + secondNumber));
+        System.out.println("Разность чисел:" + (firstNumber - secondNumber));
+        System.out.println("Произведение чисел:" + (firstNumber * secondNumber));
+        System.out.println("Частное чисел:" + ((double) firstNumber / secondNumber));
+//Запрашивается путь к файлу
+        int count = 1;
+        while (true) {
+            System.out.println("Укажите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean pathToFile = file.isDirectory();
+            if (!fileExist || pathToFile) {
+                System.out.println("Файл по указаному пути отсутствует или Вы указали путь к папке");
+                continue;
+            } else {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + count + "\n");
+                count++;
+            }
+        }
     }
 }
