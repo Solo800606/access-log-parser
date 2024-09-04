7222309c89f82642a272f53cec601738881efb92
Solo800606 <61974103+Solo800606@users.noreply.github.com>
2024-09-04 18:54:20 +0300

    Добавлена логика по работе с файлом. Получения данных о количестве строк, самой длинной строке и короткой строке.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,12 +10,12 @@ public class Main {
         int firstNumber = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число:");
         int secondNumber = new Scanner(System.in).nextInt();
-//Вывод результата
-        System.out.println("Сумма чисел:" + (firstNumber + secondNumber));
-        System.out.println("Разность чисел:" + (firstNumber - secondNumber));
-        System.out.println("Произведение чисел:" + (firstNumber * secondNumber));
-        System.out.println("Частное чисел:" + ((double) firstNumber / secondNumber));
-//Запрашивается путь к файлу
+        // Вывод результата
+        System.out.println("Сумма чисел: " + (firstNumber + secondNumber));
+        System.out.println("Разность чисел: " + (firstNumber - secondNumber));
+        System.out.println("Произведение чисел: " + (firstNumber * secondNumber));
+        System.out.println("Частное чисел: " + ((double) firstNumber / secondNumber));
+        // Запрашивается путь к файлу
         int count = 1;
         while (true) {
             System.out.println("Укажите путь к файлу");
@@ -21,12 +24,40 @@ public class Main {
             boolean fileExist = file.exists();
             boolean pathToFile = file.isDirectory();
             if (!fileExist || pathToFile) {
-                System.out.println("Файл по указаному пути отсутствует или Вы указали путь к папке");
+                System.out.println("Файл по указанному пути отсутствует или Вы указали путь к папке");
                 continue;
             } else {
                 System.out.println("Путь указан верно");
                 System.out.println("Это файл номер " + count + "\n");
                 count++;
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    int totalLines = 0;
+                    int maxLength = 0;
+                    int minLength = Integer.MAX_VALUE;
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        if (length > 1024) {
+                            throw new LineTooLongException("ОБНАРУЖЕНА строка длиннее 1024 символов!!!");
+                        }
+                        totalLines++;
+                        if (length > maxLength) {
+                            maxLength = length;
+                        }
+                        if (length < minLength) {
+                            minLength = length;
+                        }
+                    }
+                    System.out.println("Общее количество строк в файле: " + totalLines);
+                    System.out.println("Длина самой длинной строки в файле: " + maxLength);
+                    System.out.println("Длина самой короткой строки в файле: " + minLength);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (LineTooLongException ex) {
+                    System.out.println(ex.getMessage());
+                    break;
+                }
             }
         }
     }
